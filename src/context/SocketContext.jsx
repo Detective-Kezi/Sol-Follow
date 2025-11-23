ff3e8a5a1998bd0ca71b82d87b0d0d99e201c735
Detective-Kezi <91566248+Detective-Kezi@users.noreply.github.com>
2025-11-23 11:22:19 +0100

    Update SocketContext.jsx

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -76,37 +76,4 @@ export function SocketProvider({ children }) {
 
 export function useSocket() {
   return useContext(SocketContext)
-    }      setData(serverData)
-    })
-
-    newSocket.on('newTrade', (trade) => {
-      setData(prev => ({ ...prev, trades: [trade, ...prev.trades.slice(0,49)] }))
-    })
-
-    newSocket.on('sold', (sell) => {
-      setData(prev => ({ ...prev, trades: [sell, ...prev.trades] }))
-    })
-
-    newSocket.on('positionsUpdate', (positions) => {
-      setData(prev => ({ ...prev, positions }))
-    })
-
-    newSocket.on('goldenAlphasUpdate', (alphas) => {
-      setData(prev => ({ ...prev, goldenAlphas: alphas }))
-    })
-
-    setSocket(newSocket)
-
-    return () => newSocket.close()
-  }, [])
-
-  return (
-    <SocketContext.Provider value={{ socket, data, setData }}>
-      {children}
-    </SocketContext.Provider>
-  )
-}
-
-export function useSocket() {
-  return useContext(SocketContext)
-}
+      }
